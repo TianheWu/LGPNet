f58fe74e9e2ff444dcc5e15a9741e0f561b7a837
TianheWu <tianhe_wu@foxmail.com>
2022-03-02 14:21:37 +0800

    delete shell files

--- train_jacquard.sh
+++
@@ -1,9 +0,0 @@
-python train_net.py --description gpnn_r18_jacssptea_0.8-1.0 \
-    --batch-size 3 \
-    --network lgpnet \
-    --dataset jacquard \
-    --dataset-path ./dataset/dataset_jacquard/ \
-    --layers 18 \
-    --gpu-idx 7 \
-    --start-split 0.8 \
-    --end-split 1.0 
